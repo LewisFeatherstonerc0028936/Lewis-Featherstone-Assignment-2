10bb93d1512a4734b0e5b4c0a889a16be4a62d40
Lewis Featherstone <Lewis.featherstone@outlook.com>
2018-05-24 16:16:38 +0100

    began

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,14 +7,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+
+        class row
+        {
+            public double acceleration;
+            public double velocity;
+            public double altitude;
+            public double time;
+        }
+
+        List<row> table = new List<row>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "csv Files|*.csv";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    using
+                }
+            }
+
         }
     }
 }
